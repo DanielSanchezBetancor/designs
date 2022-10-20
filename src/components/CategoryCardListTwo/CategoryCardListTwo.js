119cebd57b0b649a3dd12f720d2130b2d2331e68
DSanchez <danielsanchezpro@gmail.com>
2022-10-20 18:34:32 +0200

    @Category Card List Design @Category Card List design n 2

---
+++ src/components/CategoryCardListTwo/CategoryCardListTwo.js
@@ -0,0 +1,16 @@
+import CategoryCardListItemTwo from '../CategoryCardListItemTwo/CategoryCardListItemTwo.js';
+import './CategoryCardListTwo.css';
+
+function CategoryCardListTwo() {
+    return (
+        <div className="category-card-list-two">
+            <CategoryCardListItemTwo />
+            <CategoryCardListItemTwo />
+            <CategoryCardListItemTwo />
+            <CategoryCardListItemTwo />
+            <CategoryCardListItemTwo />
+        </div>
+    );
+}
+
+export default CategoryCardListTwo;
